861ad5088a87cf8ffb5ae1aa6c975a1afb36b113
Alex Bisag <bisagalexstefan@gmail.com>
2018-11-17 22:15:20 +0200

    Koa init

---
+++ app.js
@@ -0,0 +1,25 @@
+const PORT = 3015;
+const Koa = require('koa');
+const Router = require('koa-router');
+const combineRouters = require('koa-combine-routers');
+const app = new Koa();
+const router = new Router();
+
+router.get("/", async ctx => {
+   ctx.body = "nairu back-end init";
+})
+
+router.get("/version", async ctx => {
+    ctx.body = "version 0.0.0"
+})
+
+const combinedRouters = combineRouters(
+	router
+)
+
+app.use(combinedRouters())
+
+app.listen(PORT, () => {
+   console.log('nairu back-end');
+   console.log('server started at: http://localhost:' + PORT);
+})
